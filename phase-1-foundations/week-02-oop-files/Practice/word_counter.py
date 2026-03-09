c95eb272dcdc8695a615eafea50d4cd246efb9bd
rohit-ai-engineer <rohitmasteringai@gmail.com>
2026-03-10 03:38:33 +0530

    practice: complete word frequency counter exercise

--- phase-1-foundations/week-02-oop-files/Practice/word_counter.py
+++ phase-1-foundations/week-02-oop-files/Practice/word_counter.py
@@ -1,21 +1,15 @@
-# word_count = {}
+word_count = {}
 
-# user_input = input("Enter you sentence: ")
-# word_spilt = user_input.split()
+user_input = input("Enter you sentence: ")
+word_spilt = user_input.split()
 
-# for word in word_spilt:
-#     if word in word_count:
-#         word_count [word] += 1
-#     else:
-#         word_count[word] = 1
-
-# for word, count in word_count.items():
-#     print (f"{word}: {count}") 
-
-
-word_count = {'hello': 2, 'world': 1, 'python': 1}
-
-print("Items:", word_count.items())
+for word in word_spilt:
+    if word in word_count:
+        word_count [word] += 1
+    else:
+        word_count[word] = 1
 
 for word, count in word_count.items():
-    print(f"Key: {word}, Value: {count}")
+    print (f"{word}: {count}") 
+
+
